861edf9cf72f5c3ac6d441368335e79eb6dc70b9
Igor Spichkin <igorsa.ux@outlook.com>
2023-10-20 11:53:43 +0300

    Updates

--- assets/cssMode-2357ca1b.js
+++ assets/cssMode-2357ca1b.js
@@ -1,4 +1,4 @@
-import{m as Fe}from"./index-aad63360.js";/*!-----------------------------------------------------------------------------
+import{m as Fe}from"./index-4116382b.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.44.0(3e047efd345ff102c8c61b5398fb30845aaac166)
  * Released under the MIT license
