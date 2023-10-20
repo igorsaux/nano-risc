c4b34efce8448392fb206bd0f6ab0648a1fbdf22
Igor Spichkin <igorsa.ux@outlook.com>
2023-10-20 10:11:43 +0300

    feat: PWA IDE

--- ide/vite.config.ts
+++ ide/vite.config.ts
@@ -4,10 +4,51 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import wasm from 'vite-plugin-wasm'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), wasm()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    wasm(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        theme_color: '#ff7a00',
+        background_color: '#ff7a00',
+        display: 'standalone',
+        name: 'Nano-RISC',
+        short_name: 'nRISC',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   build: {
     target: 'esnext'
   },
